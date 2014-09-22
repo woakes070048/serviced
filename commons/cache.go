bf9273c33cad12d74b1239b1e559b5703ce11213
Ian McCracken <ian.mccracken@gmail.com>
2014-09-22 10:58:07 -0500

    Rewrite some stuff to make it more pretty-like

--- commons/cache.go
+++ commons/cache.go
@@ -24,9 +24,12 @@ func (c *CopyCache) Invalidate(key string) {
 func (c *CopyCache) Add(key string, val interface{}) {
 	c.Lock()
 	defer c.Unlock()
-	// Make sure that we dereference in case val is a pointer
-	v := reflect.Indirect(reflect.ValueOf(val)).Interface()
-	c.data[key] = v
+	c.data[key] = val
+	//// Make sure that we make a copy
+	//obj := reflect.ValueOf(val)
+	//cp := reflect.New(obj.Type())
+	//cp.Elem().Set(obj)
+	//c.data[key] = cp.Elem().Interface()
 }
 
 func (c *CopyCache) Get(key string) (interface{}, bool) {
@@ -34,16 +37,24 @@ func (c *CopyCache) Get(key string) (interface{}, bool) {
 	return val, ok
 }
 
+// GetInto() gets a cached value at the specified target address.
 func (c *CopyCache) GetInto(key string, target interface{}) (bool, error) {
+	// Retrieve a cached value, if there is one
 	val, ok := c.Get(key)
 	if !ok {
 		return false, nil
 	}
+	// Verify that the target address is in fact a pointer
 	t := reflect.ValueOf(target)
 	if t.Kind() != reflect.Ptr {
 		return false, fmt.Errorf("GetInto() requires a pointer")
 	}
+	// Make sure that the value we retrieve from the cache is the object itself
 	v := reflect.ValueOf(val)
+	if v.Kind() == reflect.Interface || v.Kind() == reflect.Ptr {
+		v = v.Elem()
+	}
+	// Write a copy of the cached value to the address passed in
 	t.Elem().Set(v)
 	return true, nil
 }
